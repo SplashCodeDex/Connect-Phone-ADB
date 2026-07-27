14eb8de5720454a72a1a8cd3bcee36625efb69b1
CodeDeX <splashdexstudios@gmail.com>
2026-07-27 00:48:04 +0000

    chore: remove all remaining dead scripts and properly restore engine fixes

--- Connect Phone ADB.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-title Connect Phone ADB Launcher
-wscript.exe "C:\Users\NicoDex\Connect-Phone-ADB\bin\Start-App.vbs"
--- convert_icon.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-img = Image.open(r"C:\Users\NicoDex\.gemini\antigravity-cli\brain\2e07987f-a127-46e0-95e9-65e358d7b8a5\adb_connect_app_icon_1784852546657.jpg")
-# Ensure image is square and save as multi-size ICO
-img.save(r"C:\Users\NicoDex\Connect-Phone-ADB\assets\app-icon.ico", format="ICO", sizes=[(256, 256), (128, 128), (64, 64), (32, 32), (16, 16)])
